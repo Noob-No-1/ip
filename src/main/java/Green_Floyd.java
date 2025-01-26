cd978b7c2cfc6ff190ab99f97d980387cea07270
Noob-No-1 <155603010+Noob-No-1@users.noreply.github.com>
2025-01-26 17:54:29 +0800

    Add Task class and mark and unmark function

--- src/main/java/Green_Floyd.java
+++ src/main/java/Green_Floyd.java
@@ -5,7 +5,7 @@ public class Green_Floyd {
     public String separateBar = new String("#########################################################");
     private boolean isRunning;
 
-    private ArrayList<String> list = new ArrayList<>();
+    private ArrayList<Task> list = new ArrayList<>();
     public Green_Floyd() {
         this.isRunning = true;
     }
@@ -27,13 +27,27 @@ public class Green_Floyd {
         isRunning = false;
     }
     public void handleCommand (String input) { //handle different behaviors base on user input
-        switch (input.trim().toLowerCase()) {
+        //formatting input
+        String[] parts = input.split(" ", 2);
+        String action = parts[0].trim().toLowerCase();
+        String details = parts.length > 1 ? parts[1].trim() :"";
+        switch (action) {
             case "bye":
                 exitChat();
                 break;
             case "list":
                 printSeparateBar();
                 listTask();
+                printSeparateBar();
+                break;
+            case "mark":
+                printSeparateBar();
+                markDone(action, details);
+                printSeparateBar();
+                break;
+            case "unmark":
+                printSeparateBar();
+                undone(action, details);
                 printSeparateBar();
                 break;
             default:
@@ -47,18 +61,55 @@ public class Green_Floyd {
         System.out.println(separateBar);
     }
 
+    public void markDone(String action, String details) {
+        try {
+            int index = Integer.parseInt(details);
+            if (index < 0 || index >= list.size()) {
+                throw new IndexOutOfBoundsException("Invalid index number!");
+            }
+            Task task = list.get(index - 1);
+            task.markAsDone();
+            System.out.println("Nice! I've marked this task done!\n");
+            System.out.println("[" + task.getStatus() + "] " + task.description);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid index");
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter command with a valid task number e.g. 'mark 2'");
+        }
+    }
+
+    public void undone(String action, String details) {
+        try {
+            int index = Integer.parseInt(details);
+            if (index < 0 || index >= list.size()) {
+                throw new IndexOutOfBoundsException("Invalid index number!");
+            }
+            Task task = list.get(index - 1);
+            task.markAsUndone();
+            System.out.println("Nice! I've marked this task done!\n");
+            System.out.println("[" + task.getStatus() + "] " + task.description);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid index");
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter command with a valid task number e.g. 'mark 2'");
+        }
+    }
     public void listTask() { //List all added tasks in the list, print it in console
         int tasks = list.size();
         if (tasks == 0) {
             System.out.println("No tasks added yet! Try typing tasks to me, I will add them to list");
         } else {
             for (int i = 0; i < tasks; i++) {
-                System.out.println((i + 1) + ". " + list.get(i));
+                Task task = list.get(i);
+                String description = task.description;
+                String status = task.getStatus();
+                System.out.println((i + 1) + ". " + "[" + status + "] " + description );
             }
         }
     }
     public void addToList(String input) { //add the input task into list
-        list.add(input);
+        Task task = new Task(input);
+        list.add(task);
         System.out.println("Added: " + input);
     }
     public void greeting() { //say hello :)
