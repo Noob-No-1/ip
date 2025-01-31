c22a74e93d42be776887c3cbe64032c08e9eb3a4
Noob-No-1 <155603010+Noob-No-1@users.noreply.github.com>
2025-01-31 12:30:53 +0800

    Add time format

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -11,25 +11,32 @@ public class Events extends Task{
     public String to_str;
     public String details;
     public Events(String description, String from, String to, boolean status) {
+
         super(description, status);
         this.details = description;
         from_str = from;
         to_str = to;
         this.from = parseDateTime(from);
         this.to = parseDateTime(to);
+
     }
 
     private LocalDateTime parseDateTime(String s) {
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
         return LocalDateTime.parse(s, formatter);
+
     }
     public String toFileFormat() { //E | 0 | project meeting | Mon 2pm | 4pm
+
         return "E" + " | " + (super.isDone ? "1" : "0") + " | " + description + " | " + from_str + " | " + to_str;
     }
 
     @Override
     public String toString() {
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm a");
         return "[E]" + super.toString() + " (from: " + from.format(formatter) + " to: " + to.format(formatter) + ")";
+
     }
 }
