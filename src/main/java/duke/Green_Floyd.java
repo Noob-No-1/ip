4345192df0ef97f2b50a90175b8a21f7d2571265
Noob-No-1 <155603010+Noob-No-1@users.noreply.github.com>
2025-02-01 15:08:39 +0800

    Fix bugs on mark and unmark function

--- src/main/java/duke/Green_Floyd.java
+++ src/main/java/duke/Green_Floyd.java
@@ -98,6 +98,7 @@ public class Green_Floyd {
         int index = parseIndex(details);
         Task task = tasks.getTask(index);
         task.markAsDone();
+        storage.saveTasks(tasks.getTasks());
         System.out.println("Nice! I've marked this task as done:");
         System.out.println("  " + task);
     }
@@ -112,6 +113,7 @@ public class Green_Floyd {
         int index = parseIndex(details);
         Task task = tasks.getTask(index);
         task.markAsUndone();
+        storage.saveTasks(tasks.getTasks());
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println("  " + task);
     }
@@ -126,6 +128,7 @@ public class Green_Floyd {
         int index = parseIndex(details);
         Task task = tasks.getTask(index);
         tasks.deleteTask(index);
+        storage.saveTasks(tasks.getTasks());
         System.out.println("Noted. I've removed this task:");
         System.out.println("  " + task);
         System.out.println("Now you have " + tasks.size() + " tasks in the list.");
